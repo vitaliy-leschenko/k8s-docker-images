e99c57de2af34b93ceaadb3bdb3d6f720b5ebbc4
Vitaliy Leschenko <v.leschenko@vitaliy.org>
2020-11-07 12:16:32 +0300

    build

--- kubeadm/flannel/build.ps1
+++ kubeadm/flannel/build.ps1
@@ -4,4 +4,6 @@ $env:GOOS = "windows"
 $env:GOARCH = "amd64"
 & go build -o setup.exe setup.go
 
+ls
+
 #& docker.exe build -f .\Dockerfile -t "flannel:1809" --build-arg=flannelVersion=0.13.0 --build-arg=tag=1809 .

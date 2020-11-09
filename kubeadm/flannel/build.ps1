fef205d7d790bb3a69cea7e6169202acd86918ab
Vitaliy Leschenko <v.leschenko@vitaliy.org>
2020-11-09 16:21:37 +0300

    update build.ps1 for flannel and kubeproxy

--- kubeadm/flannel/build.ps1
+++ kubeadm/flannel/build.ps1
@@ -1,57 +1,50 @@
 param(
-    [switch] $Push, 
-    [string] $Image = "sigwindowstools/flannel"
+    [switch] $push, 
+    [string] $image = "sigwindowstools/flannel"
 )
 
 $env:DOCKER_CLI_EXPERIMENTAL = "enabled"
 & docker buildx create --name img-builder --use
 
 $output="docker"
-if ($Push.IsPresent) {
+if ($push.IsPresent) {
     $output="registry"
 }
 
-$data = Get-Content .\versions.json | ConvertFrom-Json
-$data.flannel
-foreach ($flannel in $data.flannel)
+$config = Get-Content .\buildconfig.json | ConvertFrom-Json
+$base = $config.baseimage
+foreach ($flannel in $config.flannel)
 {
     Write-Host "Build images for flannel version: $flannel"
 
-    foreach ($template in $data.baseimages)
+    $cmd = "docker manifest create $($image):$flannel"
+    foreach($map in $config.tagsMap) 
     {
-        $base = $template.base
-        $suffix = $template.suffix
-        $dockerfile = $template.dockerfile
-
-        Write-Host "$($Image):$($flannel)$($suffix)"
-
-        $cmd = "docker manifest create $($Image):$($flannel)$($suffix)"
-
-        foreach ($tag in $template.tags)
-        {
-            $cmd = "$cmd --amend $($Image):$($flannel)-$($tag)"
-
-            Write-Host "$($base):$tag"
-            & docker buildx build --pull --platform windows/amd64 --output=type=$output -f $dockerfile -t "$($Image):$($flannel)-$($tag)" --build-arg=BASE="$($base):$($tag)" --build-arg=flannelVersion=$flannel .
-        }
-
-        if ($Push.IsPresent) {
-            Write-Host $cmd
-            Invoke-Expression $cmd
-
-            foreach ($tag in $template.tags)
-            {
-                $manifest = $(docker manifest inspect "$($base):$($tag)" -v) | ConvertFrom-Json
-                $platform = $manifest.Descriptor.platform
-                $folder = ("docker.io/$($Image):$($flannel)$($suffix)" -replace "/", "_") -replace ":", "-"
-                $img = ("docker.io/$($Image):$($flannel)-$($tag)" -replace "/", "_") -replace ":", "-"
-                Write-Host "Update '~/.docker/manifests/$folder/$img'"
-                $manifest = Get-Content "~/.docker/manifests/$folder/$img" | ConvertFrom-Json
-                $manifest.Descriptor.platform = $platform
-                $manifest | ConvertTo-Json -Depth 10 -Compress | Set-Content "~/.docker/manifests/$folder/$img"
-            }
-
-            & docker manifest push "$($Image):$($flannel)$($suffix)"
-        }
+        $cmd = "$cmd --amend $($image):$flannel-$($map.target)"
+        Write-Host "Build $($image):$flannel-$($map.target)" -ForegroundColor Green
+        & docker buildx build --pull --platform windows/amd64 --output=type=$output -f Dockerfile -t "$($image):$flannel-$($map.target)" --build-arg=BASE="$($base):$($map.source)" --build-arg=flannelVersion=$flannel .
     }
+
+    if ($push.IsPresent)
+    {
+        Write-Host "Create manifest for $($image):$flannel" -ForegroundColor Yellow
+        Write-Host $cmd
+        Invoke-Expression $cmd
+
+        foreach($map in $config.tagsMap) 
+        {
+            $manifest = $(docker manifest inspect "$($base):$($map.source)" -v) | ConvertFrom-Json
+            $platform = $manifest.Descriptor.platform
+            $folder = ("docker.io/$($image):$flannel" -replace "/", "_") -replace ":", "-"
+            $img = ("docker.io/$($image):$flannel-$($map.target)" -replace "/", "_") -replace ":", "-"
+            Write-Host "Update '~/.docker/manifests/$folder/$img'"
+            $manifest = Get-Content "~/.docker/manifests/$folder/$img" | ConvertFrom-Json
+            $manifest.Descriptor.platform = $platform
+            $manifest | ConvertTo-Json -Depth 10 -Compress | Set-Content "~/.docker/manifests/$folder/$img"
+        }
+
+        & docker manifest push "$($image):$flannel"
+    }
+
+    Write-Host
 }

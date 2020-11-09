1e6ce87565ea1d578fa5256eea8c9ac5e1fae2c6
Vitaliy Leschenko <v.leschenko@vitaliy.org>
2020-11-09 15:12:08 +0300

    buildx kubeproxy

---
+++ kubeadm/kube-proxy/build.ps1
@@ -0,0 +1,47 @@
+param(
+    [string]$image = "sigwindowstools/kube-proxy",
+    [switch]$push
+)
+
+[int]$minMajor = 1
+[int]$minMinor = 17
+
+$env:DOCKER_CLI_EXPERIMENTAL = "enabled"
+& docker buildx create --name img-builder --use
+
+$output="docker"
+if ($push.IsPresent) {
+    $output="registry"
+}
+
+
+function buildKubeProxy([string]$tag) 
+{
+    Write-Host "-------------- $tag ---------------" -ForegroundColor White
+
+    $versions = Get-Content ".\buildconfig.json" | ConvertFrom-Json
+    $base = $versions.baseimage
+    foreach($map in $versions.tagsMap) 
+    {
+        Write-Host "Build $($image):$tag-$($map.target)" -ForegroundColor Green
+        & docker buildx build --pull --platform windows/amd64 --output=type=$output -f .\Dockerfile -t "$($image):$tag-$($map.target)" --build-arg=BASE="$($base):$($map.source)" --build-arg=flannelVersion=$flannel .
+    }
+
+    Write-Host "todo: Create manifest for $($image):$tag" -ForegroundColor Yellow
+    Write-Host
+}
+
+$tags = (curl -L k8s.gcr.io/v2/kube-proxy/tags/list | ConvertFrom-Json).tags
+foreach($tag in $tags)
+{
+    if ($tag -match "^v(\d+)\.(\d+)\.(\d+)$")
+    {
+        [int]$major = $Matches[1]
+        [int]$minor = $Matches[2]
+
+        if (($major -gt $minMajor) -or ($major -eq $minMajor -and $minor -ge $minMinor))
+        {
+            buildKubeProxy -tag $tag
+        }
+    }
+}
